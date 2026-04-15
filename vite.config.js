05f00da47e0399648e2f5f6e567f08afbddd8f61
Mashiro <masakinoel@hotmail.com>
2026-04-15 23:20:49 +0900

    Update icon purpose in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,27 @@ export default defineConfig({
             src: 'icon-512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'any' // ← anyのみに変更
+          },
+          {
+            src: 'icon-512.png', // 本来は専用の「icon-maskable-512.png」を用意するのがベスト
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable' // ← 分けて記述
+          }
+        ],
+        screenshots: [
+          {
+            src: 'screenshot-mobile.png',
+            sizes: '750x1334', // 実際のサイズに合わせて変更してください
+            type: 'image/png',
+            form_factor: 'narrow'
+          },
+          {
+            src: 'screenshot-desktop.png',
+            sizes: '1920x1080', // 実際のサイズに合わせて変更してください
+            type: 'image/png',
+            form_factor: 'wide'
           }
         ]
       },
